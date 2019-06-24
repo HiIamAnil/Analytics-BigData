f8c83b55d1cac4134099214c2726f53919b52c94
HiIamAnil <Just4learngh@gmail.com>
2019-06-24 15:38:28 +0530

    Query book
    This Book day today production grade queries

---
+++ QureyNotebook.sql
@@ -0,0 +1,9 @@
+# sql queries 
+
+# how to find the null count of a column using this self join left
+select count(*) from 
+( (select * from people ) a
+left outer join 
+(select id, birthdate from people) b
+on a.id = b.id)
+where b.birthdate is null
